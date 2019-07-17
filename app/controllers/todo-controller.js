eb80f5daaa76b46ac103f535c322f5e5d1c0191f
Jhoel Ccari <jhoelccari.23@gmail.com>
2019-07-17 13:48:52 -0500

    controllers

--- app/controllers/todo-controller.js
+++ app/controllers/todo-controller.js
@@ -1,18 +1,40 @@
+const ObjetctID = require('mongodb').ObjectID
+
 module.exports = function(app, db){
     return {
+        index : (req, res) => {
+            res.send("TODO raiz");
+        },
+
         list : (req, res) => {
-            // let todoList = db.collection("todo-list").find();
-            // res.send(todoList);
+            console.log("List")
+            db.collection("todo-list").find({}, {}).toArray()
+            .then(items => {
+                console.log(`Mostrrando ${items.length} documentos`)
+                items.forEach(console.log)
+                res.send(items);
+            })
+            .catch(err => console.error(`Ha ocurrido algun error: ${err}`))
+        },
+
+        detail : (req, res) => {
+            var query = {'_id': new ObjectID(req.params.id) };
+            db.collection('todo-list').findOne(query, (err, task) => {
+                if (err)  res.send("Error buscando la tarea");
+                
+				res.status(200).send(task);
+		    });
         },
 
         create : (req, res) => {
-            console.log(req.body)
+            //console.log(req.body)
             var data = {
                 owner: req.body.owner,
-                status: req.body.status
-            }    
+                status: req.body.status,
+                content: req.body.content
+            };
             db.collection("todo-list").insertOne(data, (err, res) => {
-                if ( err ) console.log(" Hubo un error guardando el elemento");
+                if ( err ) console.log("Hubo un error guardando el elemento");
                 console.log(data);
                 console.log("Insertado correctamente");
             });
@@ -20,12 +42,32 @@ module.exports = function(app, db){
             res.send( req.body.owner +" su tarea fue guardada");
         },
 
-        detail: (req, res) => {
-            //TODO
+        delete : (req, res) => {
+            console.log("Delete")
+            var query = {
+                _id: ObjectID(req.params.id)
+            };
+            db.collection("todo-list").deleteOne(query, (err,task) => {
+                if (err) res.send(500).send("Error borrando el elemento")
+                console.log(task);
+                res.status(200).send("Borrado");
+            });
         },
 
-        delete: (req, res) => {
-            //TODO
-        }   
+        edit : (req, res) => {
+            var query = {
+                _id: ObjectID(req.params.id)
+            };
+            var update = { $set: {
+                                    owner: req.params.owner,
+                                    status: req.params.status,
+                                    content: req.params.content
+                                }};
+            db.collection("todo-list").updateOne(query, update, (err, task) =>{
+                if (err) res.status(500).send("Error editando la tarea")
+                res.status(200).send("Tarea actualizada");
+            });
+
+        }
     }
 }
--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express         = require('express');
 const mongoClient     = require('mongodb').MongoClient;
 const bodyParser      = require('body-parser');
 
+
 const app   = express();
 const db    = require('./config/db');
+const routes = require('./app/todo-routes')
 const dbPort = 8081
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //var url = "mongodb+srv://jhoelccari:sd6CsvVWVvQDmr6O@cluster0-gmk8b.mongodb.net/todo-db?retryWrites=true&w=majority";
 
@@ -16,7 +19,7 @@ client.connect(err => {
     if (err) return console.log(err)
     const database = client.db("todo-list");
     // perform actions on the collection object
-    require('./app/todo-routes')(app, database);
+    routes(app, database);
     app.listen(dbPort, ()=>{
         console.log("[MongoDB] Listen on "+ dbPort +" port");
     })
